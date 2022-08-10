22a31a1526d1d644cb72f92d9a8f038b03f06c35
Your Name <you@example.com>
2022-08-10 14:52:45 +0530

    patch for custom player control

--- app/src/main/java/com/namespace/hlsplayer/videoPlayer.kt
+++ app/src/main/java/com/namespace/hlsplayer/videoPlayer.kt
@@ -1,20 +1,19 @@
 package com.namespace.hlsplayer
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.pm.ActivityInfo
-import android.media.browse.MediaBrowser
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.view.WindowManager
+import android.widget.ImageButton
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
-import com.google.android.exoplayer2.ui.PlayerControlView
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
-import com.namespace.hlsplayer.databinding.ActivityMainBinding
 
 
 class videoPlayer: Activity() {
@@ -26,26 +25,29 @@ class videoPlayer: Activity() {
     private lateinit var playerView: PlayerView
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.videoswitch)
         playerView = findViewById(R.id.playerView)
+
         var playable = intent.getStringExtra("Playable")
         Log.d("____TTT_________T___ ",(playable.toString()))
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //will hide the status bar
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //will rotate the screen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN) //will hide the status bar
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) //will rotate the screen
         if (playable != null) {
             if (playable.isNotEmpty()) {
                 url_ = playable
                 preparePlayer()
             } else {
                 url_ = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-                Toast.makeText(getBaseContext(), "Address is empty or not Valid! Playing a pre-defined Url", Toast.LENGTH_SHORT).show()
-                //   URL = Uri.parse("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8");
+                Toast.makeText(this, "Address is empty or not Valid! Playing a pre-defined Url", Toast.LENGTH_SHORT).show()
+                //   URL = Uri.parse("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")
                 preparePlayer()
             }
         }
     }
+
 
     private fun preparePlayer() {
         exoPlayer = ExoPlayer.Builder(this).build()
@@ -61,6 +63,9 @@ class videoPlayer: Activity() {
         exoPlayer?.playWhenReady = playWhenReady
         exoPlayer?.prepare()
 
+    }
+    fun clickEvent(v: View) {
+        Toast.makeText(v.getContext(), "Available quality", Toast.LENGTH_LONG).show()
     }
     private fun releasePlayer() {
         exoPlayer?.let { player ->
@@ -86,6 +91,8 @@ class videoPlayer: Activity() {
         releasePlayer()
     }
 
-
+    override fun onResume() {
+        super.onResume()
+    }
 }
 
