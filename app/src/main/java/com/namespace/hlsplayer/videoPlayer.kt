bbea92b95153bc322e87abd420372aa9726a5b6e
Your Name <you@example.com>
2022-08-11 17:36:20 +0530

    patch_on_AppBar_Player_EventListener

--- app/src/main/java/com/namespace/hlsplayer/videoPlayer.kt
+++ app/src/main/java/com/namespace/hlsplayer/videoPlayer.kt
@@ -7,11 +7,14 @@ import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageButton
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.source.TrackGroupArray
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.trackselection.MappingTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.ui.TrackSelectionDialogBuilder
 import com.google.android.exoplayer2.upstream.DataSource
@@ -24,8 +27,9 @@ const val MAX_HEIGHT = 539
 const val MAX_WIDTH = 959
 
 
-class videoPlayer: AppCompatActivity() {
+class videoPlayer: AppCompatActivity(),Player.Listener {
 
+    private lateinit var dialogBuilder: Dialog
     private lateinit var exoPlayer: SimpleExoPlayer
     private lateinit var dataSourceFactory: DataSource.Factory
     private lateinit var trackSelector: DefaultTrackSelector
@@ -36,19 +40,22 @@ class videoPlayer: AppCompatActivity() {
     private var isPlayerPlaying = true
     private  var HLS_STATIC_URL =""
     private var trackDialog: Dialog? = null
+    private lateinit var progressBar: View
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.videoswitch)
+        //retrieve url data from previous class
         var playable = intent.getStringExtra("Playable")
         Log.d("____TTT_________T___ ",(playable.toString()))
 
         //#setting screen to full
 
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN) //will hide the status bar
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) //will rotate the screen
-        // #retriving extra data from previous class
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN) //will hide the status bar
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE //will rotate the screen
+
+        //checking if received url data variable is null
         if (playable != null) {
             if (playable.isNotEmpty()) {
                 HLS_STATIC_URL = playable
@@ -64,34 +71,46 @@ class videoPlayer: AppCompatActivity() {
                     .setMimeType(MimeTypes.APPLICATION_M3U8)
                     .build()
                 Toast.makeText(this, "Address is empty or not Valid! Playing a pre-defined Url", Toast.LENGTH_SHORT).show()
-                //   URL = Uri.parse("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")
                 initPlayer()
             }
         }
 
+        //defining player view
         playerView = findViewById(R.id.playerView)
         exoQuality = playerView.findViewById(R.id.change_quality)
-
-        dataSourceFactory = DefaultDataSourceFactory(this,
-            Util.getUserAgent(this, "Hls Player"))
-
+        dataSourceFactory = DefaultDataSourceFactory(this, Util.getUserAgent(this, "Hls Player"))
         exoQuality.setOnClickListener{
             if(trackDialog == null){
                 initPopupQuality()
             }
             trackDialog?.show()
         }
-
-
+    }
+    //Listener on player
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        when(playbackState){
+            Player.STATE_BUFFERING ->{
+                progressBar.visibility =View.VISIBLE
+            }
+            Player.STATE_READY -> {
+                progressBar.visibility = View.GONE
+            }
+            Player.EVENT_PLAYER_ERROR ->{
+                Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
+
+
+
     private fun initPlayer(){
+        progressBar = findViewById(R.id.loading)
         playerView = findViewById(R.id.playerView)
         val mediaItem = MediaItem.Builder()
             .setUri(HLS_STATIC_URL)
             .setMimeType(MimeTypes.APPLICATION_M3U8)
             .build()
-
         trackSelector = DefaultTrackSelector(this)
         // When player is initialized it'll be played with a quality of MaxVideoSize to prevent loading in 1080p from the start
         trackSelector.setParameters(trackSelector.buildUponParameters().setMaxVideoSize(MAX_WIDTH,MAX_HEIGHT))
@@ -102,15 +121,6 @@ class videoPlayer: AppCompatActivity() {
             prepare()
         }
         playerView.player = exoPlayer
-
-        //Listener on player
-        exoPlayer.addListener(object: Player.Listener{
-            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
-                if(playbackState == Player.STATE_READY){
-                    exoQuality.visibility = View.VISIBLE
-                }
-            }
-        })
     }
 
     private fun releasePlayer(){
@@ -119,9 +129,7 @@ class videoPlayer: AppCompatActivity() {
         currentWindow = exoPlayer.currentWindowIndex
         exoPlayer.release()
     }
-
-
-
+    //Android life cycle
     override fun onStart() {
         super.onStart()
         if (Util.SDK_INT > 23) {
@@ -132,6 +140,7 @@ class videoPlayer: AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+
         if (Util.SDK_INT <= 23) {
             initPlayer()
             playerView.onResume()
@@ -140,6 +149,7 @@ class videoPlayer: AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
+        releasePlayer()
         if (Util.SDK_INT <= 23) {
             playerView.onPause()
             releasePlayer()
@@ -148,6 +158,7 @@ class videoPlayer: AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
+        releasePlayer()
         if (Util.SDK_INT > 23) {
             playerView.onPause()
             releasePlayer()
@@ -159,20 +170,16 @@ class videoPlayer: AppCompatActivity() {
     private fun initPopupQuality() {
         val mappedTrackInfo = trackSelector.currentMappedTrackInfo
         var videoRenderer : Int? = null
-
         if(mappedTrackInfo == null) return else exoQuality.visibility = View.VISIBLE
-
         for(i in 0 until mappedTrackInfo.rendererCount){
             if(isVideoRenderer(mappedTrackInfo, i)){
                 videoRenderer = i
             }
         }
-
         if(videoRenderer == null){
             exoQuality.visibility = View.GONE
             return
         }
-
         val trackSelectionDialogBuilder = TrackSelectionDialogBuilder(this, "Available Quality", trackSelector, videoRenderer)
         trackSelectionDialogBuilder.setTrackNameProvider{
             // Override function getTrackName
