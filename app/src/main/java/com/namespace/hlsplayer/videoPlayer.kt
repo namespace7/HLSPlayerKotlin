9b5875439c3180c1e39eb75f877d41b438fde898
Your Name <you@example.com>
2022-08-22 12:29:29 +0530

    picture in picture implementation

--- app/src/main/java/com/namespace/hlsplayer/videoPlayer.kt
+++ app/src/main/java/com/namespace/hlsplayer/videoPlayer.kt
@@ -2,13 +2,20 @@ package com.namespace.hlsplayer
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.app.PictureInPictureParams
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
@@ -28,6 +35,13 @@ const val MAX_WIDTH = 959
 
 class videoPlayer: AppCompatActivity() {
 
+    companion object {
+        @JvmField
+        val ARG_VIDEO_POSITION = "VideoActivity.POSITION"
+    }
+    var isInPipMode:Boolean = false
+    private var videoPosition:Long = 0L
+    var isPIPModeEnabled:Boolean = true //Has the user disabled PIP mode in AppOpps?
     private lateinit var exoPlayer: SimpleExoPlayer
     private lateinit var dataSourceFactory: DataSource.Factory
     private lateinit var trackSelector: DefaultTrackSelector
@@ -73,6 +87,67 @@ class videoPlayer: AppCompatActivity() {
             trackDialog?.show()
         }
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState.apply {
+            this?.putLong(ARG_VIDEO_POSITION, exoPlayer.currentPosition)
+        })
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        videoPosition = savedInstanceState!!.getLong(ARG_VIDEO_POSITION)
+    }
+
+    override fun onBackPressed(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+            && packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)
+            && isPIPModeEnabled) {
+            enterPIPMode()
+        } else {
+            super.onBackPressed()
+        }
+    }
+    override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration?) {
+        if(newConfig !=null){
+            videoPosition = exoPlayer.currentPosition
+            isInPipMode = !isInPictureInPictureMode
+        }
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+    }
+    //Called when the user touches the Home or Recents button to leave the app.
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        enterPIPMode()
+    }
+    @Suppress("DEPRECATION")
+    fun enterPIPMode(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+            && packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)) {
+            videoPosition = exoPlayer.currentPosition
+            binding.playerView.useController = false
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val params = PictureInPictureParams.Builder()
+                this.enterPictureInPictureMode(params.build())
+            } else {
+                this.enterPictureInPictureMode()
+            }
+            /** We need to check this because the system permission check is publically hidden for integers for non-manufacturer-built apps
+               https://github.com/aosp-mirror/platform_frameworks_base/blob/studio-3.1.2/core/java/android/app/AppOpsManager.java#L1640
+               ********* If we didn't have that problem *********
+                val appOpsManager = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+                if(appOpsManager.checkOpNoThrow(AppOpManager.OP_PICTURE_IN_PICTURE, packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA).uid, packageName) == AppOpsManager.MODE_ALLOWED)
+                30MS window in even a restricted memory device (756mb+) is more than enough time to check, but also not have the system complain about holding an action hostage.
+             */
+            Handler().postDelayed({checkPIPPermission()}, 30)
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun checkPIPPermission(){
+        isPIPModeEnabled = isInPictureInPictureMode
+        if(!isInPictureInPictureMode){
+            onBackPressed()
+        }
     }
 
 
@@ -172,18 +247,12 @@ class videoPlayer: AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-exoPlayer.playWhenReady = true
-
+        exoPlayer.playWhenReady = true
     }
 
     override fun onPause() {
         super.onPause()
-        exoPlayer.playWhenReady =false
-        releasePlayer()
-        if (Util.SDK_INT <= 23) {
-            binding.playerView.onPause()
-            releasePlayer()
-        }
+        videoPosition = exoPlayer.currentPosition
     }
 
     override fun onStop() {
@@ -193,6 +262,12 @@ exoPlayer.playWhenReady = true
             binding.playerView.onPause()
             releasePlayer()
         }
+        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            && packageManager.hasSystemFeature(
+                PackageManager.FEATURE_PICTURE_IN_PICTURE)) {
+            finishAndRemoveTask()
+        }
+
     }
 
     // QUALITY SELECTOR
